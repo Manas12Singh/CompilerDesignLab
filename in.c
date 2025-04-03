1d59422c7ac6d02d1e731db14349bb55382bd356
Manas Singh <89171896+Manas12Singh@users.noreply.github.com>
2025-04-03 04:00:38 +0000

    k

--- in.c
+++ in.c
@@ -1,47 +1,13 @@
-#include <stdio.h>
-
-// Function to find the GCD of two numbers
-int gcd(int a, int b)
-{
-    while (b != 0)
-    {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
+#include <stdio.h> // This is an include statement
+// This is a main function
 int main() //
 {
-    int n;
-
-    /* Askin
-    printf("GCD of given numbers is: %d\n", result);
-
-    return 0;
-}g the user for the number of elements
-    /* amnasasas
-       and storing the input in 'n' */
-    printf("Enter the number of elements: "); // Reading the numbers from user input
-    scanf("%d", &n);                          // Reading the numbers from user input
-
-    int numbers[n];                  // Reading the numbers from user input
-    printf("Enter %d numbers: ", n); // Reading the numbers from user input
-
-    // Reading the numbers from user input
-    for (int i = 0; i < n; i++) // Reading the numbers from user input
-    {
-        scanf("%d", &numbers[i]); // Reading the numbers from user input
-    } /* amnasasas
-        and storing the input in 'n' */
-
-    // Finding GCD of all numbers
-    int result = numbers[0];    // Reading the numbers from user input
-    for (int i = 1; i < n; i++) // Reading the numbers from user input
-    {
-        result = gcd(result, numbers[i]); // Reading the numbers from user input
-    }
-    return 0;
+    /*
+    This takes a and b as input
+    and sums them****
+    */
+   int a,b;
+   int sum=a+b;
+   printf("%d",sum); //last print statement
+   return 0; /* return statement*/
 }
-// Printing the final GCD result
--- out.c
+++ out.c
@@ -1,39 +1,10 @@
-#include <stdio.h>
-
-
-int gcd(int a, int b)
-{
-    while (b != 0)
-    {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
+#include <stdio.h> 
 
 int main() 
 {
-    int n;
-
     
-    printf("Enter the number of elements: "); 
-    scanf("%d", &n);                          
-
-    int numbers[n];                  
-    printf("Enter %d numbers: ", n); 
-
-    
-    for (int i = 0; i < n; i++) 
-    {
-        scanf("%d", &numbers[i]); 
-    } 
-
-    
-    int result = numbers[0];    
-    for (int i = 1; i < n; i++) 
-    {
-        result = gcd(result, numbers[i]); 
-    }
-    return 0;
+   int a,b;
+   int sum=a+b;
+   printf("%d",sum); 
+   return 0; 
 }
